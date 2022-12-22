2450d17afe9691e67d86addee868e86ebe34e31b
Gabriel <gabrielzuli22@gmail.com>
2022-12-22 16:19:00 -0300

    Criando formulario modelo para o movel

--- helpers/formClasses.py
+++ helpers/formClasses.py
@@ -1,5 +1,29 @@
 from flask_wtf import FlaskForm
+from wtforms import IntegerField, FloatField, validators
 
 
 class MovelForm(FlaskForm):
-    pass
+    andar = IntegerField('Andar', [validators.DataRequired()])
+    area_total = FloatField('Area do imóvel', [validators.DataRequired()])
+    banheiros = IntegerField('Banheiros', [validators.DataRequired()])
+    quartos = IntegerField('Quartos', [validators.DataRequired()])
+    suites = IntegerField('Suites', [validators.DataRequired()])
+    vaga = IntegerField('Vaga', [validators.DataRequired()])
+    condominio = FloatField('Valor comdomínio', [validators.DataRequired()])
+    iptu = FloatField('Valor IPTU', [validators.DataRequired()])
+    
+
+    # 'Academia',
+    # 'Animais permitidos',
+    # 'Churrasqueira',
+    # 'Condomínio fechado',
+    # 'Elevador',
+    # 'Piscina',
+    # 'Playground',
+    # 'Portaria 24h',
+    # 'Portão eletrônico',
+    # 'Salão de festas',
+    # 'Zona Central',
+    # 'Zona Norte',
+    # 'Zona Oeste',
+    # 'Zona Sul'
