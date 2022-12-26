72f35ae418e8ff7fe3becd2cd3b14bd11b5379a2
Gabriel <gabrielzuli22@gmail.com>
2022-12-26 15:52:09 -0300

    Criando DataFrame Spark com dados do usuario

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash
 from helpers.formClasses import ImovelForm
 from pyspark.sql import SparkSession
+from pyspark.sql.types import IntegerType, FloatType, StringType, ArrayType, StructField, StructType
 
 
 SECRET_KEY = 'development'
@@ -10,7 +11,6 @@ app.config.from_object(__name__)
 spark = SparkSession.builder \
     .master('local[*]') \
     .appName("Iniciando com Spark") \
-    .config('spark.ui.port', '4050') \
     .getOrCreate()
 
 @app.route('/imovel')
@@ -25,17 +25,27 @@ def prever():
     if not form.validate_on_submit():
         print("Validation Failed")
         print(form.errors)
+        flash('Preencha todos os campos obrigatórios')
         return render_template('imovel.html', form=form, titulo='Imóvel')
     
-    dados = [form.andar.data, form.area_total.data, form.banheiros.data,
-                form.quartos.data, form.suites.data, form.condominio.data,
-                form.iptu.data, form.caracteristicas.data, form.localizacao.data]
-    headers = ['andar', 'area_total', 'area_util',
-                'banheiros', 'quartos', 'suites',
-                'vaga', 'condominio', 'iptu',
-                'caracteristicas', 'localizacao']
-    df = spark.createDataFrame(dados, headers)
+    
+    types_df = [IntegerType(), FloatType(), IntegerType(), 
+                IntegerType(), IntegerType(), IntegerType(), FloatType(),
+                FloatType(), ArrayType(StringType(), True), StringType()]
+    headers = ['andar', 'area_total', 'banheiros',
+                'quartos', 'suites', 'vaga', 'condominio',
+                'iptu', 'caracteristicas', 'localizacao']
+    schema = StructType(
+            [StructField(header, type_df, True) for header, type_df in zip(headers, types_df)]
+        )
+    
+    dados = [(form.andar.data, form.area_total.data, form.banheiros.data,
+                form.quartos.data, form.suites.data, form.vaga.data, form.condominio.data,
+                form.iptu.data, form.caracteristicas.data, form.localizacao.data)]
+
+    df = spark.createDataFrame(dados, schema)
     print(df)
+    return "<h1> Sucesso </h1>"
 
 @app.route('/')
 def index():
